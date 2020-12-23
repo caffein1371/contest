948ef6d859517c20247d5d06ddb89a6b883714e6
cahein <incidents1371@gmail.com>
2020-12-24 01:03:31 +0900

    20201224

--- ABC139/C-Lower.py
+++ ABC139/C-Lower.py
@@ -1,26 +1,15 @@
-def checklist(List,i,ans):
-  print ('i='+str(i))
-  #print (List[i])
-  if i+1<len(List):
-    if List[i+1]<=List[i]:
-      ans +=1
-      return checklist(List,i+1,ans+1)
-    else:
-      print (ans)
-      return ans
-  else:
-    print (ans)
-    return ans
-
 N = int(input())
 Hlist = list(map(int,input().split()))
-print (len(Hlist))
 
+ans = 0
 maxnum = 0
-for i in range(len(Hlist)):
-  ans =0
-  checklist(Hlist,i,ans)
-  #print (ans)
+for i in range(len(Hlist)-1):
+  if Hlist[i+1]<=Hlist[i]:
+      #print ('{} {}'.format(Hlist[j],Hlist[j+1]))
+      ans+=1
+  else:
+    ans = 0
   if maxnum<=ans:
-    maxnum =ans
+    maxnum = ans
+  
 print (maxnum)
